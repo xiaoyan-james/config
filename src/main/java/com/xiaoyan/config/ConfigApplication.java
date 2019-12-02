1a72212c1a05d60bc70646a5e7f5aa139c8c39e5
yanguojun <779691112@qq.com>
2019-12-02 17:51:02 +0800

    增加hystrix降级依赖

--- src/main/java/com/xiaoyan/config/ConfigApplication.java
+++ src/main/java/com/xiaoyan/config/ConfigApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableConfigServer
 public class ConfigApplication {
 
