efe86a2b42998e5580c958712a248a0abe66dfd1
yanguojun <779691112@qq.com>
2019-12-06 14:00:21 +0800

    update

--- src/main/java/com/xiaoyan/config/ConfigApplication.java
+++ src/main/java/com/xiaoyan/config/ConfigApplication.java
@@ -2,10 +2,8 @@ package com.xiaoyan.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableEurekaClient
